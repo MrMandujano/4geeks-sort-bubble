d9998aa06265d4151a922f69c9a47856734a6528
Francisco Mandujano <93405601+MrMandujano@users.noreply.github.com>
2022-07-07 14:35:49 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,182 @@
+window.onload = function () {
+
+    let old_element = document.getElementById("rollBtn");
+let new_element = old_element.cloneNode(true);
+old_element.parentNode.replaceChild(new_element, old_element);
+    const suits = [
+        {
+            name: 'Hearts',
+            color: '#d63031',
+            symbol: '♥'
+        },
+        {
+            name: 'Spades',
+            color: '#1e272e',
+            symbol: '♠'
+        },
+        {
+            name: 'Clubs',
+            color: '#1e272e',
+            symbol: '♣'
+        },
+        {
+            name: 'Diamonds',
+            color: '#d63031',
+            symbol: '♦'
+        }
+    ]
+
+    const generateDeck = () => {
+        const deck = []
+        suits.forEach(suit => {
+            for (let c = 2; c <= 14; c++) {
+                deck.push({ ...suit, value: c, renderValue: cardRenderValue(c) })
+            }
+        });
+
+        return deck
+    }
+    
+    const cardRenderValue = (val) => {
+        if (val === 11) {
+            return 'J'
+        }
+        if (val === 12) {
+            return 'Q'
+        }
+        if (val === 13) {
+            return 'K'
+        }
+        if (val === 14) {
+            return 'A'
+        }
+
+        return `${val}`
+    }
+
+    const generateRandomIndex = (max) => {
+        min = 0
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    const generateRandomCards = () => {
+        let times = document.getElementById('inputRandom').value || 0
+        if(times > 52){
+            times = 52
+        }
+        const arr = []
+        for(let cards = 1; cards <= times; cards++){
+            const randomIndex = generateRandomIndex(deck.length - 1)
+            arr.push(deck[randomIndex])
+            deck.splice(randomIndex, 1)
+        }
+
+        return arr
+    
+    }
+    
+    const renderCards = (arr) =>{
+        const container = document.getElementById('deckRender')
+        arr.forEach(card => {
+            const cardDiv = `<div class="card">
+            <div class="symbol top-left" style="color: ${card.color}">${card.symbol}</div>
+            <div class="symbol bottom-right" style="color: ${card.color}">${card.symbol}</div>
+            <div class="card-render-value" style="color: ${card.color}">${card.renderValue}</div>
+        </div>`
+            container.insertAdjacentHTML("beforeend", cardDiv);
+        });
+    }
+
+    const renderLogs = (rows) => {
+        const logs = document.getElementById('logsList')
+       
+
+        console.log(rows,'<--')
+
+        const card = (card) => {
+            return `<div class="card">
+            <div class="symbol top-left" style="color: ${card.color}">${card.symbol}</div>
+            <div class="symbol bottom-right" style="color: ${card.color}">${card.symbol}</div>
+            <div class="card-render-value" style="color: ${card.color}">${card.renderValue}</div>
+        </div>`
+            
+        }
+        rows.forEach((row,index) => {
+            let logRow = document.createElement('div');
+            const h6 = document.createElement('h4')
+            h6.innerHTML = `${index}.`
+            h6.style.padding= '1em'
+            logRow.appendChild(h6)
+            logRow.classList.add('logRow')
+
+            row.forEach(c => {
+                const appendCard = card(c)
+                logRow.insertAdjacentHTML("beforeend", appendCard);
+            })
+            logs.appendChild(logRow)
+        
+        }) 
+    }
+
+    const sortItems = (array) => {
+        let swapped = true;
+        let logs = []
+        do {
+            let asd = [...array]
+                logs.push(asd)
+            swapped = false;
+            
+            for (let j = 0; j < array.length; j++) {
+                
+                if (array[j + 1] && array[j].value > array[j + 1].value) {
+                    
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+        return logs;
+    }
+
+    const clearCards = () => {
+        const container = document.getElementById('deckRender')
+        while (container.firstChild) {
+            container.firstChild.remove()
+        }
+    }
+
+    const clearSorted = () => {
+        const container = document.getElementById('logsList')
+        while (container.firstChild) {
+            container.firstChild.remove()
+        }
+    }
+    
+    document.getElementById('rollBtn').addEventListener('click', function(){
+        clearCards()
+        clearSorted()
+        deck = generateDeck()
+        cards = generateRandomCards()
+        renderCards(cards)
+
+    })
+
+    document.getElementById('sortBtn').addEventListener('click', function(){
+       const logRows = sortItems([...cards])
+       console.log(logRows)
+       renderLogs(logRows)
+        
+    })
+
+   
+    let deck = generateDeck()
+    let cards = generateRandomCards()
+   
+
+    renderCards(cards)
+    
+}
